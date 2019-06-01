c1f0e09873d520ab2e583f40a838ed3177dc690f
Zdravko Zdravkov <pirocorp@abv.bg>
2019-06-01 17:22:35 +0200

    Demo for instanceof operator

---
+++ 01. JAVASCRIPT SYNTAX AND OPERATORS/Lab/00.Demo/Instanceof.js
@@ -0,0 +1,8 @@
+//The instanceof operator returns true if the specified object
+//is an instance of the specified object:
+
+let cars = ["Saab", "Volvo", "BMW"]; 
+console.log(cars instanceof Array);		    // Returns true
+console.log(cars instanceof Object);		// Returns true
+console.log(cars instanceof String);		// Returns false
+console.log(cars instanceof Number);		// Returns false
