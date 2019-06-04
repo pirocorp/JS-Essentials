fa380957d78aed9059ae51f0340a545c0f669ca8
Zdravko Zdravkov <pirocorp@abv.bg>
2019-06-04 17:16:32 +0200

    Bug fix

--- 01.SYNTAX, FUNCTIONS AND STATEMENTS/Exercise/02. Syntax, Functions and Statements/03. Validity Checker.js
+++ 01.SYNTAX, FUNCTIONS AND STATEMENTS/Exercise/02. Syntax, Functions and Statements/03. Validity Checker.js
@@ -20,7 +20,7 @@ function solve(cordinates) {
         if (distance % 1 !== 0) {
             console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is invalid`)
         } else {
-            console.log(`{${x2}, ${y2}} to {${x2}, ${y2}} is valid`)
+            console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is valid`)
         }
     }
 
@@ -29,4 +29,4 @@ function solve(cordinates) {
     }
 }
 
-solve([2, 1, 1, 1])
+solve([3, 0, 0, 4])
